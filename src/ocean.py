2f4e09f111a7d2f6f6aa4547e7546a57ecbfe953
bauduf <flavien.baudu@grenoble-inp.org>
2023-04-14 16:08:28 +0200

    commented print that made the simulation crash

--- src/ocean.py
+++ src/ocean.py
@@ -25,7 +25,7 @@ class Ocean(Mesh):
         for i in range(-symSizeMesh, symSizeMesh + 1):
             for j in range(-symSizeMesh, symSizeMesh + 1):
                 position.append(np.array([i * scale, heightOffset, j * scale], dtype='f'))
-        print(position)
+        # print(position)
 
         # Index creation
         index = []
@@ -41,7 +41,7 @@ class Ocean(Mesh):
 
         self.color = (.2, .2, 1)
         attributes = dict(position=position)
-        print(attributes)
+        # print(attributes)
         super().__init__(shader, attributes=attributes, index=index, global_color=self.color, time=0.0, light=lightDir, k_d=(0.0000, 0.6000, 0.6000), k_a=(0.0000, 0.2000, 0.2000), k_s=(0.8, 0.8, 0.8), s=100.0000)
 
     def draw(self, **_args):
